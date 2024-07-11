a2b8c50898d1e9ee3e3de56e1dfe87288a407633
flawstick <amirashak101@gmail.com>
2024-07-11 11:25:55 +0300

    Adding forced token verification

--- src/lib/hooks/useAuth.ts
+++ src/lib/hooks/useAuth.ts
@@ -9,6 +9,9 @@ const useAuth = () => {
   useEffect(() => {
     if (status === "loading") return; // Do nothing while loading
     if (!session) router.push("/login"); // Redirect to login if not authenticated
+
+    // @ts-ignore
+    if (session?.status === "unverified") router.push("/login"); // Redirect to verify-request if not verified
   }, [session as any, status, router]);
 
   useEffect(() => {
